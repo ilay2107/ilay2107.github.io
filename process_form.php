db3be589c64968c310101aa6a7fdfd5c1fde9bbb
Ilya Rohoutseu <01184619@pw.edu.pl>
2023-08-19 16:24:11 +0300

    Add file

--- process_form.php
+++ process_form.php
@@ -2,29 +2,13 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = $_POST["name"];
     $phone = $_POST["phone"];
-    
-    $chat_id = "798768713"; // Замените на ваш chat_id
-    $bot_token = "6589231306:AAENatTz3oLLRpMQIBCT-4SMZ-4MYYaqHpg"; // Замените на ваш токен
 
+    // Отправка письма на почту
+    $to = "rogovtsevilia@gmail.com"; // Замените на ваш адрес электронной почты
+    $subject = "Новое сообщение с формы";
     $message = "Имя: $name\nТелефон: $phone";
+    $headers = "From: webmaster@example.com";
 
-    $url = "https://api.telegram.org/bot$bot_token/sendMessage";
-    $params = [
-        'chat_id' => $chat_id,
-        'text' => $message
-    ];
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    if ($response) {
-        echo "Спасибо за отправку!";
-    } else {
-        echo "Произошла ошибка при отправке.";
-    }
+    mail($to, $subject, $message, $headers);
 }
 ?>
